2ee67f2bb75011b9f9c7aa0908ba0fb57e228db5
David Blanchard <dpjb@hotmail.fr>
2021-05-26 18:06:40 +0200

    Implement the base page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,19 @@
-@extends('header_and_footer.main');
+@extends('header_and_footer.main')
 @section('title')
-    Welcome home!
+Les prix bas !
+@stop
+
+@section('stylesheets')
+    <link rel="stylesheet" href="/css/index.css" />
+    <link rel="stylesheet" href="/css/carousel.css" />
+@stop
+
+@section('head_javascripts')
+@stop
+
+@section('children')
+    @include('product_page.main')
+@stop
+
+@section('body_javascripts')
 @stop
