a134ed51de6912f9f3a1638da78676d69d744913
David Blanchard <dpjb@hotmail.fr>
2021-05-29 10:28:06 +0200

    Update user nav with auth actions

--- resources/views/user/user_nav_button.blade.php
+++ resources/views/user/user_nav_button.blade.php
@@ -1,14 +1,37 @@
 @php
-    $home = $_SERVER['REQUEST_URI'];
+    $home = route('home');
+    $login = "Connexion"; // __('Login')
+    $register = "Pas encore inscrit ?"; // __('Register')
+    $logout = "Déconnexion"; // __('Logout')
 @endphp
 
 <button type="button" class="btn btn-secondary dropdown-toggle my-2 my-sm-0 ml-3"  id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    @guest
     <i class="fa fa-user"></i>
+    @else
+        {{ Auth::user()->name }}
+    @endguest
 </button>
 <div class="dropdown-menu dropdown-menu-right" aria-labelledby='dropdown01'>
-    <button class='dropdown-item' href='{{ $home }}'>Connexion</button>
-    <button class='dropdown-item' href='{{ $home }}'>Pas encore inscrit ?</button>
+    @guest
+        @if (Route::has('login'))
+            <a class="dropdown-item" href="{{ route('login') }}">{{ $login }}</a>
+        @endif
+
+        @if (Route::has('register'))
+            <a class="dropdown-item" href="{{ route('register') }}">{{ $register }}</a>
+        @endif
+    @else
+        <a class="dropdown-item" href="{{ route('logout') }}" 
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            {{ $logout }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    @endguest
+
     <button class='dropdown-item' href='{{ $home }}'>F.A.Q</button>
     <button class='dropdown-item' href='{{ $home }}'>Mentions légales</button>
 </div>
-
