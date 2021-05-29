7deeb04b0435bce161cdb27155ad3ed819bebce6
David Blanchard <dpjb@hotmail.fr>
2021-05-29 13:21:28 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::redirect('/home', '/', 301);
+Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::redirect('/', '/product', 301);
+
+Route::resource('/products', ProductsController::class);
 
 Auth::routes();
-
