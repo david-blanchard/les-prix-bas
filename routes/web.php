337179961ebc7e79cb59fab321e3d028c780a9e8
David Blanchard <dpjb@hotmail.fr>
2021-05-29 10:27:16 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::redirect('/home', '/', 301);
 
-Route::get('/', [MainController::class, 'home']);
+Auth::routes();
+
