74e3c71ca2ebf5ee31e38d97a7cc6decf5006b3c
David Blanchard <dpjb@hotmail.fr>
2021-06-08 16:52:35 +0200

    Fix price in cart

--- resources/views/product_info/top_panel.blade.php
+++ resources/views/product_info/top_panel.blade.php
@@ -1,14 +1,20 @@
+@php
+    $hasDiscount = $props->discount > 0;
+    $priceTag = $hasDiscount ? $props->discount : $priceTag = $props->price;
+@endphp
 <div class="row">
     <div class=""> {{ $props->name }} </div>
 </div>
 <div class="row justify-content-between">
     <div class=""> {{ $props->brand }} </div>
     <div class="">
-        <div id="price-tag" name="unit-price" class='badge badge-light sl-1'><del> {{ $props->price }}&nbsp; <i class="fa fa-euro"></i> </del></div>
+    @if($hasDiscount)
+        <div id="stroke-price-tag" name="unit-price" class='badge badge-light sl-1'><del> {{ $props->price }}&nbsp; <i class="fa fa-euro"></i> </del></div>
+    @endif
         <div id="price-tag" name="unit-price">
             <button type="button" class="btn btn-success">
                 <span class="">
-                    {{ $props->discount }}&nbsp;<i class="fa fa-euro"></i>
+                    {{ $priceTag }}&nbsp;<i class="fa fa-euro"></i>
                 </span>
             </button>
         </div>
