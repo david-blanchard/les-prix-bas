a653872a759aeb32ec1f0f5970c700b0b370a954
David Blanchard <dpjb@hotmail.fr>
2021-06-08 10:58:03 +0200

    Update routes for data management

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\BrandsController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImagesController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductImagesController;
 use App\Http\Controllers\ProductsController;
-use Facade\FlareClient\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +25,8 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', [ProductController::class, 'index'])->name('root');
-Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::get('/product', [ProductController::class, 'index'])->name('default');
+Route::get('/product/{slug}', [ProductController::class, 'show'])->name('product');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 // Route::redirect('/', '/product', 301);
 
@@ -33,12 +36,25 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/admin', function() {
     return View('admin.main');
 })->name('admin');
-Route::get('/admin/products', [ProductsController::class, 'index'])->name('products_man');
 
-/**
- * Products API routes
- */
-Route::resource('/products', ProductsController::class)->middleware('admin');
+Route::get('/admin/products', [ProductsController::class, 'index'])->middleware('admin')->name('products_man');
+Route::get('/admin/products/create', [ProductsController::class, 'create'])->middleware('admin')->name('products_man.create');
+Route::post('/admin/products/store', [ProductsController::class, 'store'])->middleware('admin')->name('products_man.store');
+Route::get('/admin/products/{product}/edit', [ProductsController::class, 'edit'])->middleware('admin')->name('products_man.edit');
+Route::put('/admin/products/{product}/update', [ProductsController::class, 'update'])->middleware('admin')->name('products_man.update');
+Route::delete('/admin/products/{product}/destroy', [ProductsController::class, 'destroy'])->middleware('admin')->name('products_man.delete');
+
+Route::get('/admin/brands', [BrandsController::class, 'index'])->middleware('admin')->name('brands_man');
+Route::get('/admin/brands/create', [BrandsController::class, 'create'])->middleware('admin')->name('brands_man.create');
+Route::post('/admin/brands/store', [BrandsController::class, 'store'])->middleware('admin')->name('brands_man.store');
+
+Route::get('/admin/images', [ImagesController::class, 'index'])->middleware('admin')->name('images_man');
+Route::get('/admin/images/create', [ImagesController::class, 'create'])->middleware('admin')->name('images_man.create');
+Route::post('/admin/images/store', [ImagesController::class, 'store'])->middleware('admin')->name('images_man.store');
+
+Route::get('/admin/product_images', [ProductImagesController::class, 'index'])->middleware('admin')->name('product_images_man');
+Route::get('/admin/product_images/create/{productId}', [ProductImagesController::class, 'create'])->middleware('admin')->name('product_images_man.create');
+Route::post('/admin/product_images/store', [ProductImagesController::class, 'store'])->middleware('admin')->name('product_images_man.store');
 
 /**
  * Authentication routes
