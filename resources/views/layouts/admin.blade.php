a89f23c0bc9b7292ef06fdeecb121ce414d6aadc
David Blanchard <dpjb@hotmail.fr>
2021-06-12 12:07:11 +0200

    Separate CSS from base templates

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,81 +11,8 @@
     <link rel="stylesheet" href="/assets/css/font-awesome.min.css" />
     <link rel="stylesheet" href="/assets/dist/css/bootstrap.css" />
     <link rel="stylesheet" href="/css/footer.css" />
-    <link rel="stylesheet" href="/css/index.css" />
+    <link rel="stylesheet" href="/css/admin.css" />
 
-
-    <style>
-         .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
- 
-
-        @media (min-width: 1280px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-            #breadcrumb0 {
-                position: sticky;
-                /* padding-top: 31px; */
-                float: left;
-            }
-
-            #subnavbar1 {
-                /* */
-            }
-            #placeholder {
-                /* top: 88px; */
-                height: 74px;
-            }
-        }
-
-        @media (max-width: 420px) {
-            #cart-cta {
-                bottom: 88px;
-                position: relative;
-                left: 50%;
-            }
-            #dropdown01 {
-                bottom: 88px;
-                position: relative;
-                left: 47%;
-            }
-            #navbar0 {
-                height: 88px;
-            }
-            #subnavbar0 {
-                height: 44px;
-            }
-
-            #breadcrumb0 {
-                position: sticky;
-                /* padding-top: 55px; */
-                float: left;
-            }
-            #subnavbar1 {
-                padding-top: 10px;
-            }
-            #placeholder {
-                top: 88px;
-                height: 78px;
-            }
-            #form0 {
-                width: 100%;
-            }
-        }
-    </style>
     @yield('stylesheets')
     <script src="/assets/js/vendor/jquery.slim.min.js"></script>
     <script src="/assets/dist/js/bootstrap.bundle.js"></script>
